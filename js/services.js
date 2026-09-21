e9548e39b64ce0c80436d3919d604901995a9a30
pepeda <121668381+pepeda1@users.noreply.github.com>
2026-09-21 21:03:21 +0100

    Refactor displayServices function for clarity

--- js/services.js
+++ js/services.js
@@ -42,7 +42,7 @@ function displayServices() {
 
 		const priceLink = document.createElement("a");
 		priceLink.className = "services___list___item___boxes___price";
-		priceLink.href = `prices.html#${service.service_title.replace(/\s/g, "")}`;
+		priceLink.href = `../prices/#${service.service_title.replace(/\s/g, "")}`;
 		priceLink.innerHTML = "View prices <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"5 5 32 14\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"1\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"services___list___item___boxes___price___image\"><path d=\"M5 12h30\"/><path d=\"m29 5 7 7-7 7\"/></svg>";
 
 		reviewHeader.append(reviewName, reviewDate);
